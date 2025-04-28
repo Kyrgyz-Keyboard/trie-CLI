a5901aeccfa5bf56612e1716b92d7377312276c9
boubelpi <113375173+boubelpi@users.noreply.github.com>
2025-04-29 00:24:23 +0200

    Bugs fixed

--- run_cli.py
+++ run_cli.py
@@ -4,21 +4,21 @@ from typing import List
 from process_trie import TextProcessor
 
 cli = typer.Typer()
-text_processor = TextProcessor()
 
 @cli.command()
 def load(file_path):
-    with open(file_path, 'r') as f:
-        text = f.read()
-    text_processor.load(text)
     typer.echo(f"Text loaded successfully")
-    with open("processor_state.pkl", "wb") as q:
-        pickle.dump(text_processor, q)
+    with open("text_state.pkl", "wb") as q:
+        pickle.dump(file_path, q)
 
 @cli.command()
 def hints(words: List[str] = typer.Argument(..., show_default=False)):
-    with open("processor_state.pkl", "rb") as q:
-        cur_processor = pickle.load(q)
+    with open("text_state.pkl", "rb") as q:
+        file = pickle.load(q)
+    with open(file, "r") as f:
+        text = f.read()
+    cur_processor = TextProcessor()
+    cur_processor.load(text)
     ans = cur_processor.get_likeliest_next_words(' '.join(words))
     typer.echo(f"Here are the hints:")
     typer.echo(ans)
